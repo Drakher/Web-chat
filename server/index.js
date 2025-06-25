852684e02e80e6ad7f8bb338169a2eeb9d863e71
Vitor Augusto dos Santos <vitor.augusto1@aluno.ifsp.edu.br>
2025-06-25 00:52:49 -0300

    back deploy update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const io = require('socket.io')(server, {
   cors: { origin: 'http://localhost:5173' }
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 io.on('connection', socket => {
   console.log('User connected!', socket.id);
